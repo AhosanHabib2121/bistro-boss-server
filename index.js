2c2e454b0eee2ee5bfb48f91dff89ace8c977351
ahosanhabib2121 <ahosanhabib21021@gmail.com>
2023-11-14 21:52:04 +0600

    cartsCollection delete added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 
 
 // middleware
@@ -52,6 +52,13 @@ async function run() {
         app.post('/api/carts', async (req, res) => {
             const cartItem = req.body;
             const result = await cartsCollection.insertOne(cartItem);
+            res.send(result);
+        })
+
+        app.delete('/api/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartsCollection.deleteOne(query);
             res.send(result);
         })
 
